2627df8f2a4699229ca69f99ae75aa7800bae644
DangAn <phamdangan869@gmail.com>
2025-10-12 10:55:48 +0700

    firt com

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
