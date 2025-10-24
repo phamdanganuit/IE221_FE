c3aa00cae00f38e73cf38fed04955e23c61011b7
tuongvy <22521701@gm.uit.edu.vn>
2025-10-24 16:11:15 +0700

    Fix FE

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,14 +8,26 @@ function LoginPage() {
         className="flex w-full h-full bg-white max-md:flex-col"
       aria-label="Trang đăng nhập Wild Step"
     >
-      <div className="hidden md:flex w-full h-full justify-center items-center">
+      <div className="hidden md:flex aspect-[0.7] h-screen justify-center items-center">
         <img
           src={bg}
           alt="Login background"
-          className="w-full h-full object-cover"
+          className="h-full object-contain "
         />
       </div>
+      <button
+          onClick={() => navigate("/")}
+          className="absolute right-10 top-8 py-5 max-w-full cursor-pointer"
+        >
+          <img
+            src="/Logo.svg"
+            alt="Wild Step Logo"
+            className="object-contain w-full aspect-[9.52] max-md:w-[15rem]"
+          />
+        </button>
+      <div className="flex w-full h-screen justify-center items-center">
       <LoginForm />
+      </div>
     </div>
   );
 }
